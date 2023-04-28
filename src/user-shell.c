ec4ab853a83b1ea60ca94d9c710054b2fce36fd0
varrazha <varrazhazzandraabrar11mia7@gmail.com>
2023-04-28 18:27:13 +0700

    3.2.2 try

--- src/user-shell.c
+++ src/user-shell.c
@@ -18,6 +18,50 @@ void syscall(uint32_t eax, uint32_t ebx, uint32_t ecx, uint32_t edx) {
     __asm__ volatile("int $0x30");
 }
 
+
+
+// syscall untuk mengubah current working directory
+void chdir(const char *path) {
+    syscall(10, (uint32_t) path, 0, 0);
+}
+
+// syscall untuk menampilkan isi current working directory
+void ls(void) {
+    syscall(11, 0, 0, 0);
+}
+
+// syscall untuk membuat folder baru
+void mkdir(const char *path) {
+    syscall(12, (uint32_t) path, 0, 0);
+}
+
+// syscall untuk menampilkan isi file ke layar
+void cat(const char *path) {
+    syscall(13, (uint32_t) path, 0, 0);
+}
+
+// syscall untuk menyalin file atau folder
+void cp(const char *src_path, const char *dst_path) {
+    syscall(14, (uint32_t) src_path, (uint32_t) dst_path, 0);
+}
+
+// syscall untuk menghapus file atau folder
+void rm(const char *path) {
+    syscall(15, (uint32_t) path, 0, 0);
+}
+
+// syscall untuk memindahkan dan merename file atau folder
+void mv(const char *src_path, const char *dst_path) {
+    syscall(16, (uint32_t) src_path, (uint32_t) dst_path, 0);
+}
+
+// syscall untuk mencari file atau folder dengan nama yang sama di seluruh file system
+void whereis(const char *name) {
+    syscall(17, (uint32_t) name, 0, 0);
+}
+
+
+
 int main(void) {
     struct ClusterBuffer cl           = {0};
     struct FAT32DriverRequest request = {
